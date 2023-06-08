497433b7b78c6cd8330ad7ad98905fe52867f5f4
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-06-09 02:43:39 +0700

    fix: slicing responsive & change ext.

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -11,14 +11,14 @@ function Login() {
     
     <>
       <section className='flex h-screen'>
-        <div className='overflow-hidden relative w-[60%] flex bg-[#F0592C] justify-center items-center'>
+        <div className='overflow-hidden relative md:w-[60%] min-h-screen hidden md:flex bg-[#F0592C] justify-center items-center'>
           <div className='absolute mt-[110px] self-start w-[5000px] left-[-540px]'>
             <Image src={loginVector} 
                 alt="login-vector" 
                 width="1650" 
                 height="535" />
           </div>
-          <div className='h-[80%] w-[50%] flex flex-col justify-center items-start gap-4'>
+          <div className='h-[80%] min-[1311px]:w-[50%] max-[1310px]:w-[392px] flex flex-col justify-center items-start gap-4'>
             <div className='font-extrabold text-[40px] text-base-100 text-xl'>chiper<span className='text-[#804242] font-black'>Pay</span></div>
             <div className='self-center'>
               <div className='relative'>
@@ -30,47 +30,43 @@ function Login() {
             </div>
             <div className='font-bold text-[24px]'>App that Covering Banking Needs.</div>
             <div className='font-light text-[16px]'>
-                FazzPay is an application that focussing in banking needs for all users
+                ChiperPay is an application that focussing in banking needs for all users
               in the world. Always updated and always following world trends.
-              5000+ users registered in FazzPay everyday with worldwide
+              5000+ users registered in ChiperPay everyday with worldwide
               users coverage.
             </div>
 
           </div>
         </div>
-        <div className='w-[40%] bg-[#FBE0D8] flex justify-center items-center'>
-          <div className='h-[80%] w-[60%] flex flex-col justify-center items-start gap-8 text-black'>
+        <div className='w-[40%] bg-[#FBE0D8] flex max-[768px]:flex-1 h-screen justify-center items-center'>
+          <div className='h-[80%] max-[1022px]:w-[276px] min-[1023px]:w-[60%] max-[768px]:w-[80%] flex flex-col justify-center items-start gap-8 text-black'>
             <div className='font-bold text-[24px] text-left'>
               Start Accessing Banking Needs
               With All Devices and All Platforms
               With 30.000+ Users
             </div>
             <div>
-              Transfering money is eassier than ever, you can access FazzPay wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!
+              Transfering money is easier than ever, you can access ChiperPay wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!
             </div>
             <div className='w-full mt-8'>
               <form className='flex flex-col gap-4'>
                 <div className='flex flex-col justify-center gap-12'>
-                  <div className='flex input input-bordered border-primary relative'>
-                    <div className='flex items-center'>
-                      <HiOutlineMail className="absolute text-[#9CA3AF]" alt="Email Icon" />
-                      <input className='flex-1 w-full h-full ml-[30px] pl-[30px] bg-[#FBE0D8]' type='text' name='email' placeholder='Email' />
-                    </div>
+                  <div className='flex items-center'>
+                    <HiOutlineMail className="absolute ml-4 text-[#9CA3AF]" alt="Email Icon" />
+                    <input className='input input-bordered border-primary flex-1 w-full pl-[50px] bg-[#FBE0D8]' type='email' name='email' placeholder='Email' />
                   </div>
-                  <div className='flex input input-bordered border-primary relative'>
-                    <div className='flex items-center'>
-                      <HiLockClosed className='absolute text-[#9CA3AF]'/>
-                      <input className='input input-bordered border-primary ml-[30px] bg-[#FBE0D8]' type='password' name='password' placeholder='Password'/>
-                    </div>
+                  <div className='flex items-center'>
+                    <HiLockClosed className='absolute ml-4 text-[#9CA3AF]' alt="Password Icon"/>
+                    <input className='input input-bordered border-primary flex-1 w-full pl-[50px] bg-[#FBE0D8]' type='password' name='password' placeholder='Password'/>
                   </div>
                 </div>
                 <Link href="/auth/forgotPassword" className='text-end font-bold hover:text-primary'>Forgot Password?</Link>
-                <button type='submit' className='btn bg-[#F0592C] text-white w-full mt-12'>
+                <button type='submit' className='btn bg-[#F0592C] text-white w-full mt-6'>
                   Login
                 </button>
               </form>
+              <div className='text-center mt-8'>Don&rsquo;t have an account? Let&rsquo;s <Link href="/auth/register" className='hover:text-primary font-bold text-[#F0592C]'>Sign Up</Link></div>
             </div>
-            <div></div>
           </div>
 
         </div>
